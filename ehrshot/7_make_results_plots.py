df365c770511996bc72f28b29000c39330e6a9d7
Michael <mwornow98@gmail.com>
2024-06-02 00:37:40 +0000

    make html tables

--- ehrshot/7_make_results_plots.py
+++ ehrshot/7_make_results_plots.py
@@ -6,14 +6,14 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 from utils import (
-    LABELING_FUNCTIONS, 
+    LABELING_FUNCTION_2_PAPER_NAME, 
+    TASK_GROUP_2_PAPER_NAME,
     TASK_GROUP_2_LABELING_FUNCTION,
-    HEAD_2_NAME,
-    MODEL_2_NAME, 
+    HEAD_2_INFO,
+    MODEL_2_INFO, 
     SHOT_STRATS,
     SCORE_MODEL_HEAD_2_COLOR,
     filter_df,
-    get_rel_path,
     type_tuple_list,
 )
 from plot import (
@@ -40,17 +40,16 @@ def plot_all_labeling_functions(df_results: pd.DataFrame,
                                     score,
                                     model_heads=model_heads,
                                     is_x_scale_log=is_x_scale_log,
-                                    is_std_bars=False if labeling_function == 'chexpert' else is_std_bars,
-                                    path_to_output_table=os.path.join(path_to_output_dir, f"individual_tasks_{labeling_function}_{score}.csv"))
+                                    is_std_bars=False if labeling_function == 'chexpert' else is_std_bars)
 
     # Create a unified legend for the entire figure
-    _plot_unified_legend(fig, axes)
+    _plot_unified_legend(fig, axes, ncol=2, fontsize=8)
 
     # Plot aesthetics
     fig.suptitle(f'{score.upper()} by Task', fontsize=16)
     plt.tight_layout()
-    plt.subplots_adjust(top=0.95, bottom=0.05)
-    plt.savefig(os.path.join(path_to_output_dir, f"individual_tasks_{score}.png"), dpi=300)
+    plt.subplots_adjust(top=0.95, bottom=0.1)
+    plt.savefig(os.path.join(path_to_output_dir, f"tasks_{score}.png"), dpi=300)
     plt.close('all')
     return fig
 
@@ -68,17 +67,16 @@ def plot_all_task_groups(df_results: pd.DataFrame,
                             task_group, 
                             score,
                             model_heads=model_heads,
-                            is_x_scale_log=is_x_scale_log,
-                            path_to_output_table=os.path.join(path_to_output_dir, f"grouped_tasks_{task_group}_{score}.csv"))
+                            is_x_scale_log=is_x_scale_log)
     
     # Create a unified legend for the entire figure
-    _plot_unified_legend(fig, axes, ncol=2, fontsize=12)
+    _plot_unified_legend(fig, axes, ncol=2, fontsize=8)
 
     # Plot aesthetics
     fig.suptitle(f'{score.upper()} by Task Group', fontsize=16)
     plt.tight_layout()
     plt.subplots_adjust(top=0.92, bottom=0.1, hspace=0.25)
-    plt.savefig(os.path.join(path_to_output_dir, f"grouped_tasks_{score}.png"), dpi=300)
+    plt.savefig(os.path.join(path_to_output_dir, f"taskgroups_{score}.png"), dpi=300)
     plt.close('all')
     return fig
 
@@ -103,7 +101,7 @@ def plot_all_task_group_box_plots(df_results: pd.DataFrame,
         Patch(
             facecolor=SCORE_MODEL_HEAD_2_COLOR[score][model][head], 
             edgecolor=SCORE_MODEL_HEAD_2_COLOR[score][model][head], 
-            label=f'{MODEL_2_NAME[model]}+{HEAD_2_NAME[head]}'
+            label=f"{MODEL_2_INFO[model]['label']}+{HEAD_2_INFO[head]['label']}"
         ) 
         for (model, head) in df_[['model', 'head']].drop_duplicates().itertuples(index=False)
     ]
@@ -118,16 +116,32 @@ def plot_all_task_group_box_plots(df_results: pd.DataFrame,
     return fig
 
 
+def merge_html_tables(path_to_output_dir: str):
+    # Merge together all HTML tables for easy copying
+    html_contents = []
+    for file in os.listdir(path_to_output_dir):
+        if file.endswith('_pretty.html'):
+            name = file.replace('_pretty.html', '')
+            name = LABELING_FUNCTION_2_PAPER_NAME[name] if name in LABELING_FUNCTION_2_PAPER_NAME else (TASK_GROUP_2_PAPER_NAME[name] if name in TASK_GROUP_2_PAPER_NAME else name)
+            table_html = open(os.path.join(path_to_output_dir, file), 'r').read()
+            table_html = table_html.replace('\n', '')
+            html_contents.append(f'<h5>{name}</h5>\n' + table_html)
+    with open(os.path.join(path_to_output_dir, 'merged.html'), 'w') as fd:
+        for c in html_contents:
+            fd.write(c + '\n\n')
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Make plots of results")
-    parser.add_argument("--path_to_results_dir", default=get_rel_path(__file__, '../EHRSHOT_ASSETS/outputs/'), type=str, help="Path to directory containing results from 7_eval.py")
-    parser.add_argument("--path_to_output_dir", default=get_rel_path(__file__, '../EHRSHOT_ASSETS/figures/'), type=str, help="Path to directory to save figures")
+    parser.add_argument("--path_to_labels_and_feats_dir", required=True, type=str, help="Path to directory containing saved labels and featurizers")
+    parser.add_argument("--path_to_results_dir", required=True, type=str, help="Path to directory containing results from 7_eval.py")
+    parser.add_argument("--path_to_output_dir", required=True, type=str, help="Path to directory to save figures")
     parser.add_argument("--shot_strat", required=True, type=str, choices=SHOT_STRATS.keys(), help="What type of k-shot evaluation we are interested in.")
     parser.add_argument("--model_heads", type=type_tuple_list, default=[], help="Specific (model, head) combinations to plot. Format it as a Python list of tuples of strings, e.g. [('clmbr', 'lr'), ('count', 'gbm')]")
     return parser.parse_args()
-
+    
 if __name__ == "__main__":
     args = parse_args()
+    PATH_TO_LABELS_AND_FEATS_DIR: str = args.path_to_labels_and_feats_dir
     PATH_TO_RESULTS_DIR: str = args.path_to_results_dir
     PATH_TO_OUTPUT_DIR: str = args.path_to_output_dir
     SHOT_STRAT: str = args.shot_strat
@@ -136,14 +150,14 @@ if __name__ == "__main__":
     
     # Load all results from CSVs
     dfs: List[pd.DataFrame] = []
-    for idx, labeling_function in tqdm(enumerate(LABELING_FUNCTIONS)):
-        path_to_csv: str = os.path.join(PATH_TO_RESULTS_DIR, f"{labeling_function}/{SHOT_STRAT}_results.csv")
-        if os.path.exists(path_to_csv):
-            dfs.append(pd.read_csv(path_to_csv))
-        else:
-            print(f"Skipping: {labeling_function} b/c no file at `{path_to_csv}`")
+    for idx, labeling_function in tqdm(enumerate(LABELING_FUNCTION_2_PAPER_NAME.keys())):
+        path_to_csv = os.path.join(PATH_TO_RESULTS_DIR, f"{labeling_function}/{SHOT_STRAT}_results.csv")
+        if not os.path.exists(path_to_csv): 
+            print("Skipping ", labeling_function)
+            continue
+        dfs.append(pd.read_csv(path_to_csv))
     df_results: pd.DataFrame = pd.concat(dfs, ignore_index=True)
-    df_results.to_csv(os.path.join(PATH_TO_OUTPUT_DIR, 'all_results.csv'))
+
     
     ####################################
     ####################################
@@ -190,11 +204,11 @@ if __name__ == "__main__":
     #   Rows = model + head
     #   Columns = k
     #   Cells = mean ± std of score
-    for sub_task in df_means['sub_task'].unique():
-        for score in df_means['score'].unique():
-            path_to_output_dir_: str = os.path.join(PATH_TO_OUTPUT_DIR, 'individual_tasks', score)
+    for score in df_means['score'].unique():
+        path_to_output_dir_: str = os.path.join(PATH_TO_OUTPUT_DIR, 'individual_tasks', score)
+        for sub_task in df_means['sub_task'].unique():
             os.makedirs(path_to_output_dir_, exist_ok=True)
-            df_ = filter_df(df_means, sub_tasks=[sub_task], score=score).sort_values(by=['model', 'head', 'k'])
+            df_ = filter_df(df_means, sub_tasks=[sub_task], score=score, model_heads=MODEL_HEADS).sort_values(by=['model', 'head', 'k'])
             df_ = df_.rename(columns = {'value' : 'mean' })
             df_std_ = df_stds[(df_stds['sub_task'] == sub_task) & (df_stds['score'] == score)].sort_values(by=['model', 'head', 'k'])
             df_['std'] = df_std_['value']
@@ -205,18 +219,30 @@ if __name__ == "__main__":
             df_['value'] = df_['mean'].round(3).astype(str) + ' ± ' + df_['std'].round(3).astype(str)
             df_ = df_.drop(columns=['mean', 'std'])
             df_ = df_.pivot(index=['model', 'head'], columns='k', values='value').reset_index()
+            df_.columns = [ str(x) for x in df_.columns ]
+            df_ = df_.rename(columns={'-1' : 'All'})
             df_.to_csv(os.path.join(path_to_output_dir_, f'{sub_task}_pretty.csv'), index=False)
+            # Create HTML Table with multicolumn header
+            df_['model'] = df_['model'] + ' - ' + df_['head']
+            df_ = df_.drop(columns=['head'])
+            df_.columns = pd.MultiIndex.from_tuples([
+                ('Model', ''),
+                ('All', ''),
+            ] + [ ('K', x) for x in df_.columns[2:] ])
+            df_.to_html(os.path.join(path_to_output_dir_, f'{sub_task}_pretty.html'), classes=['leaderboard_table'], index=False)
+        # Merge together all HTML tables for easy copying
+        merge_html_tables(path_to_output_dir_)
 
     # Table for each (task group, score)
     #   Rows = model + head
     #   Columns = k
     #   Cells = mean ± std of score
     task_groups: List[str] = list(TASK_GROUP_2_LABELING_FUNCTION.keys())
-    for task_group in task_groups:
-        for score in df_means['score'].unique():
-            path_to_output_dir_: str = os.path.join(PATH_TO_OUTPUT_DIR, 'task_groups', score)
+    for score in df_means['score'].unique():
+        path_to_output_dir_: str = os.path.join(PATH_TO_OUTPUT_DIR, 'task_groups', score)
+        for task_group in task_groups:
             os.makedirs(path_to_output_dir_, exist_ok=True)
-            df_ = filter_df(df_means, task_group=task_group, score=score)
+            df_ = filter_df(df_means, task_group=task_group, score=score, model_heads=MODEL_HEADS)
             # Do another round of averaging over all subtasks:
             df_ = df_.groupby([
                 'model',
@@ -239,8 +265,20 @@ if __name__ == "__main__":
             df_['value'] = df_['mean'].round(3).astype(str)
             df_ = df_.drop(columns=['mean', ])
             df_ = df_.pivot(index=['model', 'head'], columns='k', values='value').reset_index()
+            df_.columns = [ str(x) for x in df_.columns ]
+            df_ = df_.rename(columns={'-1' : 'All'})
             df_.to_csv(os.path.join(path_to_output_dir_, f'{task_group}_pretty.csv'), index=False)
-
+            # Create HTML Table with multicolumn header
+            df_['model'] = df_['model'] + ' - ' + df_['head']
+            df_ = df_.drop(columns=['head'])
+            df_.columns = pd.MultiIndex.from_tuples([
+                ('Model', ''),
+                ('All', ''),
+            ] + [ ('K', x) for x in df_.columns[2:] ])
+            df_.to_html(os.path.join(path_to_output_dir_, f'{task_group}_pretty.html'), classes=['leaderboard_table'], index=False)
+        # Merge together all HTML tables for easy copying
+        merge_html_tables(path_to_output_dir_)
+    
     ####################################
     ####################################
     #
@@ -248,7 +286,9 @@ if __name__ == "__main__":
     #
     ####################################
     ####################################
-    
+
+    print("Plotting Models: ", MODEL_HEADS)
+
     # Plotting individual AUROC/AUPRC plot for each labeling function
     for score in tqdm(df_results['score'].unique(), desc='plot_all_labeling_functions()'):
         if score == 'brier': continue
@@ -261,9 +301,8 @@ if __name__ == "__main__":
         plot_all_task_groups(df_results, score, path_to_output_dir=PATH_TO_OUTPUT_DIR, 
                              model_heads=MODEL_HEADS, is_x_scale_log=True)
 
-    # Plotting aggregated auroc and auprc box plots by task groups as box plots
+    # plotting aggregated auroc and auprc box plots by task groups
     for score in tqdm(df_results['score'].unique(), desc='plot_all_task_group_box_plots()'):
         if score == 'brier': continue
         plot_all_task_group_box_plots(df_results, score, path_to_output_dir=PATH_TO_OUTPUT_DIR,
                                       model_heads=MODEL_HEADS)
-    
