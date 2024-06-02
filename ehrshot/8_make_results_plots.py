68aaa16710dadd2ea699c554885e35a60429bf1c
Michael <mwornow98@gmail.com>
2024-06-02 00:23:34 +0000

    tables for html

--- ehrshot/8_make_results_plots.py
+++ ehrshot/8_make_results_plots.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 from utils import (
     LABELING_FUNCTION_2_PAPER_NAME, 
+    TASK_GROUP_2_PAPER_NAME,
     TASK_GROUP_2_LABELING_FUNCTION,
     HEAD_2_INFO,
     MODEL_2_INFO, 
@@ -115,6 +116,20 @@ def plot_all_task_group_box_plots(df_results: pd.DataFrame,
     return fig
 
 
+def merge_html_tables(path_to_output_dir: str):
+    # Merge together all HTML tables for easy copying
+    html_contents = []
+    for file in os.listdir(path_to_output_dir):
+        if file.endswith('_pretty.html'):
+            name = file.replace('_pretty.html', '')
+            name = LABELING_FUNCTION_2_PAPER_NAME[name] if name in LABELING_FUNCTION_2_PAPER_NAME else (TASK_GROUP_2_PAPER_NAME[name] if name in TASK_GROUP_2_PAPER_NAME else name)
+            table_html = open(os.path.join(path_to_output_dir, file), 'r').read()
+            table_html = table_html.replace('\n', '')
+            html_contents.append(f'<h6>{name}</h6>\n' + table_html)
+    with open(os.path.join(path_to_output_dir, 'merged.html'), 'w') as fd:
+        for c in html_contents:
+            fd.write(c + '\n\n')
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Make plots of results")
     parser.add_argument("--path_to_labels_and_feats_dir", required=True, type=str, help="Path to directory containing saved labels and featurizers")
@@ -142,6 +157,135 @@ if __name__ == "__main__":
             continue
         dfs.append(pd.read_csv(path_to_csv))
     df_results: pd.DataFrame = pd.concat(dfs, ignore_index=True)
+
+    
+    ####################################
+    ####################################
+    #
+    # Tables
+    #
+    ####################################
+    ####################################
+    
+    df_means = df_results.groupby([
+        'labeling_function',
+        'sub_task',
+        'model',
+        'head',
+        'score',
+        'k',
+    ]).agg({
+        'value' : 'mean',
+        'k' : 'first',
+        'labeling_function' : 'first',
+        'sub_task' : 'first',
+        'model' : 'first',
+        'head' : 'first',
+        'score' : 'first',
+    }).reset_index(drop = True)
+    df_stds = df_results.groupby([
+        'labeling_function',
+        'sub_task',
+        'model',
+        'head',
+        'score',
+        'k',
+    ]).agg({
+        'value' : 'std',
+        'k' : 'first',
+        'labeling_function' : 'first',
+        'sub_task' : 'first',
+        'model' : 'first',
+        'head' : 'first',
+        'score' : 'first',
+    }).reset_index(drop = True).fillna(0)
+    
+    # Table for each (labeling function, score)
+    #   Rows = model + head
+    #   Columns = k
+    #   Cells = mean ± std of score
+    for score in df_means['score'].unique():
+        path_to_output_dir_: str = os.path.join(PATH_TO_OUTPUT_DIR, 'individual_tasks', score)
+        for sub_task in df_means['sub_task'].unique():
+            os.makedirs(path_to_output_dir_, exist_ok=True)
+            df_ = filter_df(df_means, sub_tasks=[sub_task], score=score, model_heads=MODEL_HEADS).sort_values(by=['model', 'head', 'k'])
+            df_ = df_.rename(columns = {'value' : 'mean' })
+            df_std_ = df_stds[(df_stds['sub_task'] == sub_task) & (df_stds['score'] == score)].sort_values(by=['model', 'head', 'k'])
+            df_['std'] = df_std_['value']
+            # Save raw df
+            df_.to_csv(os.path.join(path_to_output_dir_, f'{sub_task}_raw.csv'), index=False)
+            # Save pretty df
+            df_ = df_.drop(columns = ['score', 'sub_task', 'labeling_function'])
+            df_['value'] = df_['mean'].round(3).astype(str) + ' ± ' + df_['std'].round(3).astype(str)
+            df_ = df_.drop(columns=['mean', 'std'])
+            df_ = df_.pivot(index=['model', 'head'], columns='k', values='value').reset_index()
+            df_.columns = [ str(x) for x in df_.columns ]
+            df_ = df_.rename(columns={'-1' : 'All'})
+            df_.to_csv(os.path.join(path_to_output_dir_, f'{sub_task}_pretty.csv'), index=False)
+            # Create HTML Table with multicolumn header
+            df_['model'] = df_['model'] + ' - ' + df_['head']
+            df_ = df_.drop(columns=['head'])
+            df_.columns = pd.MultiIndex.from_tuples([
+                ('Model', ''),
+                ('All', ''),
+            ] + [ ('K', x) for x in df_.columns[2:] ])
+            df_.to_html(os.path.join(path_to_output_dir_, f'{sub_task}_pretty.html'), classes=['leaderboard_table'], index=False)
+        # Merge together all HTML tables for easy copying
+        merge_html_tables(path_to_output_dir_)
+
+    # Table for each (task group, score)
+    #   Rows = model + head
+    #   Columns = k
+    #   Cells = mean ± std of score
+    task_groups: List[str] = list(TASK_GROUP_2_LABELING_FUNCTION.keys())
+    for score in df_means['score'].unique():
+        path_to_output_dir_: str = os.path.join(PATH_TO_OUTPUT_DIR, 'task_groups', score)
+        for task_group in task_groups:
+            os.makedirs(path_to_output_dir_, exist_ok=True)
+            df_ = filter_df(df_means, task_group=task_group, score=score, model_heads=MODEL_HEADS)
+            # Do another round of averaging over all subtasks:
+            df_ = df_.groupby([
+                'model',
+                'head',
+                'k',
+            ]).agg({
+                'value' : 'mean',
+                'k' : 'first',
+                'labeling_function' : 'first',
+                'sub_task' : 'first',
+                'model' : 'first',
+                'head' : 'first',
+                'score' : 'first'
+            }).reset_index(drop = True)
+            df_ = df_.rename(columns = {'value' : 'mean' })
+            # Save raw df
+            df_.to_csv(os.path.join(path_to_output_dir_, f'{task_group}_raw.csv'), index=False)
+            # Save pretty df
+            df_ = df_.drop(columns = ['score', 'sub_task', 'labeling_function'])
+            df_['value'] = df_['mean'].round(3).astype(str)
+            df_ = df_.drop(columns=['mean', ])
+            df_ = df_.pivot(index=['model', 'head'], columns='k', values='value').reset_index()
+            df_.columns = [ str(x) for x in df_.columns ]
+            df_ = df_.rename(columns={'-1' : 'All'})
+            df_.to_csv(os.path.join(path_to_output_dir_, f'{task_group}_pretty.csv'), index=False)
+            # Create HTML Table with multicolumn header
+            df_['model'] = df_['model'] + ' - ' + df_['head']
+            df_ = df_.drop(columns=['head'])
+            df_.columns = pd.MultiIndex.from_tuples([
+                ('Model', ''),
+                ('All', ''),
+            ] + [ ('K', x) for x in df_.columns[2:] ])
+            df_.to_html(os.path.join(path_to_output_dir_, f'{task_group}_pretty.html'), classes=['leaderboard_table'], index=False)
+        # Merge together all HTML tables for easy copying
+        merge_html_tables(path_to_output_dir_)
+    
+    ####################################
+    ####################################
+    #
+    # Plots
+    #
+    ####################################
+    ####################################
 
     print("Plotting Models: ", MODEL_HEADS)
 
