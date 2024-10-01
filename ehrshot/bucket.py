f7b1942870461a6b0246edf49f818b36e68b6107
MiguelAFH <miguel.angel.fh@hotmail.com>
2024-09-30 19:29:06 -0700

    Update bucket.py

--- ehrshot/bucket.py
+++ ehrshot/bucket.py
@@ -29,6 +29,35 @@ from hf_ehr.utils import load_tokenizer_from_path, load_model_from_path
 from starr_eda import calc_n_gram_count, calc_inter_event_times
 
 
+def calculate_brier_score_per_tercile(df, strat_column, strat_method, model, task, part=None):
+    results = []
+    df.drop_duplicates(inplace=True)
+
+    # Rank the strat_column and divide into terciles
+    df['tercile'] = pd.qcut(df[strat_column].rank(method='first'), 3, labels=False)
+
+    for tercile in df['tercile'].unique():
+        tercile_df = df[df['tercile'] == tercile]
+
+        # Calculate Brier score for each tercile, even if there's only one label
+        try:
+            brier_score = brier_score_loss(tercile_df['y'], tercile_df['pred_proba'])
+        except ValueError as e:
+            print(f"Error calculating Brier score for tercile {tercile + 1}: {e}")
+            continue
+        
+        results.append({
+            'model': model,
+            'task': task,
+            'strat_method': strat_method,
+            'strat_metric': strat_column if not part else f'{strat_column}_{part}',
+            'tercile': f'{tercile + 1}',
+            'brier_score': brier_score
+        })
+    
+    return results
+
+
 def calculate_brier_score_per_quartile(df, strat_column, strat_method, model, task, part=None):
     results = []
     df.drop_duplicates(inplace=True)
@@ -66,14 +95,6 @@ def parse_args():
 
 
 if __name__ == '__main__':
-    
-    # 1. You need to load the stratify.py outputs for this TASK
-    # 2. You need to load the EHRSHOT results for this TASK
-    # 3. You need to load the labeled patients for this TASK
-    # 4. You need to split the patients into 4 buckets based on their df_stratify metrics
-        # i.e. 0-25, 25-50, 50-75, 75-100th percentiles
-    # 5. Recalculate AUROC within each bucket
-    
     args = parse_args()
     print("-" * 50)
     print(f"Computing stratified brier score for task: {args.task}, model: {args.model}.")
@@ -82,7 +103,6 @@ if __name__ == '__main__':
     # Constants
     PATH_TO_DATABASE: str = '/share/pi/nigam/mwornow/ehrshot-benchmark/EHRSHOT_ASSETS/femr/extract'
     PATH_TO_FEATURES_DIR: str = '/share/pi/nigam/mwornow/ehrshot-benchmark/EHRSHOT_ASSETS/features_ehrshot'
-    # PATH_TO_RESULTS_DIR: str = '/share/pi/nigam/mwornow/ehrshot-benchmark/EHRSHOT_ASSETS/results_ehrshot'
     PATH_TO_RESULTS_DIR: str = '/share/pi/nigam/migufuen/ehrshot-benchmark/EHRSHOT_ASSETS/results_ehrshot'
     PATH_TO_TOKENIZED_TIMELINES_DIR: str = '/share/pi/nigam/mwornow/ehrshot-benchmark/EHRSHOT_ASSETS/tokenized_timelines_ehrshot'
     PATH_TO_LABELS_DIR: str = '/share/pi/nigam/mwornow/ehrshot-benchmark/EHRSHOT_ASSETS/benchmark_ehrshot'
@@ -101,7 +121,6 @@ if __name__ == '__main__':
 
     # Get features for patients
     model: str = 'gpt2-base-512--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last'
-    # model: str = 'mamba-tiny-16384--clmbr_train-tokens-total_nonPAD-ckpt_val=2000000000-persist_chunk:last_embed:last'
     patient_ids, label_values, label_times, feature_matrixes = get_labels_and_features(labeled_patients, 
                                                                                         PATH_TO_FEATURES_DIR, 
                                                                                         PATH_TO_TOKENIZED_TIMELINES_DIR,
@@ -129,7 +148,6 @@ if __name__ == '__main__':
     df_preds['pid_idx'] = train_pids_idx + val_pids_idx + test_pids_idx
     df_preds['label_time'] = label_times
     df_preds = df_preds[df_preds['pid_idx'].isin(test_pids_idx)]
-
     strats = {
         'inter_event_times': {
             'strat_cols': ['time'],
@@ -139,7 +157,7 @@ if __name__ == '__main__':
             'strat_cols': ['rr_1', 'rr_2', 'rr_3', 'rr_4', 'rr_10', 'rr', 'ttr']
         }, 
         'timeline_lengths': {
-            'strat_cols': ['n_events']
+            'strat_cols': ['n_events', 'n_unique_events', 'n_tokens', 'n_unique_tokens']
         }
     }
     results_df = pd.DataFrame()
@@ -147,7 +165,6 @@ if __name__ == '__main__':
         strat_cols = metrics['strat_cols']
         sep = metrics.get('sep', None)
         df = pd.read_parquet(os.path.join(path_to_output_dir, f'df__{args.task}__{strat}__metrics.parquet'))
-        # breakpoint()
         for strat_col in strat_cols:
             # Merge df_preds with df based on pid
             if strat_col not in df.columns:
@@ -155,7 +172,6 @@ if __name__ == '__main__':
                 continue
             if sep:
                 for part in df['metric'].unique().tolist():
-                    # breakpoint()
                     df2 = df[df['metric'] == part]
                     merged_df = pd.merge(df_preds, df2[['pid', strat_col]], on=['pid'])
                     strat_results = calculate_brier_score_per_quartile(merged_df, strat_col, strat, args.model, args.task, part)
