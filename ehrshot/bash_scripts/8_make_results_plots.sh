317fc7ec465f586705eccfe8f22be9f0d1404d4f
Stefan Hegselmann <stefan.hegselmann@charite.de>
2025-01-06 13:28:03 +0100

    Used custom make results plots.

--- ehrshot/bash_scripts/8_make_results_plots.sh
+++ ehrshot/bash_scripts/8_make_results_plots.sh
@@ -9,9 +9,12 @@
 
 mkdir -p ../../EHRSHOT_ASSETS/figures
 
+# --model_heads "[('clmbr', 'lr_lbfgs'), ('llm', 'lr_lbfgs'), ('agr', 'lr_lbfgs'), ('count', 'lr_lbfgs'), ('count', 'gbm'), ('count', 'rf')]" \
+# --path_to_results_dir ../../EHRSHOT_ASSETS/results \
+
 python3 ../8_make_results_plots.py \
     --path_to_labels_and_feats_dir ../../EHRSHOT_ASSETS/benchmark \
-    --path_to_results_dir ../../EHRSHOT_ASSETS/results \
+    --path_to_results_dir ../../EHRSHOT_ASSETS/experiments/markdown_serialization_final/gteqwen2_7b_instruct_unqiue_then_list_visits_wo_allconds_w_values_4k_no_labs_single_true_false_true_3_full_with_baselines \
     --path_to_output_dir ../../EHRSHOT_ASSETS/figures \
-    --model_heads "[('clmbr', 'lr_lbfgs'), ('llm', 'lr_lbfgs'), ('agr', 'lr_lbfgs'), ('count', 'lr_lbfgs'), ('count', 'gbm'), ('count', 'rf')]" \
+    --model_heads "[('count', 'gbm'), ('clmbr', 'lr_lbfgs'), ('llm', 'lr_lbfgs')]" \
     --shot_strat all
